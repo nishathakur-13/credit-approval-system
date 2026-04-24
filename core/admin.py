94a33103515ed841a56eff47e143ba1f3434ff3f
Nisha Kumari <kumarinis2002@gmail.com>
2026-04-24 21:39:51 +0530

    Register models in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Customer, Loan
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'monthly_salary', 'approved_limit')
+    search_fields = ('first_name', 'last_name', 'phone_number')
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'loan_amount', 'interest_rate', 'tenure')
+    list_filter = ('interest_rate',)
